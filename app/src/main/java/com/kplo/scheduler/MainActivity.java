8ca6abc175a487a4a7eaf4be1a0f438198c7337f
yeon7485 <wsx4379@naver.com>
2021-07-23 21:50:31 +0900

    calendar create, toolbar setting

--- app/src/main/java/com/kplo/scheduler/MainActivity.java
+++ app/src/main/java/com/kplo/scheduler/MainActivity.java
@@ -2,13 +2,30 @@ package com.kplo.scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView hello_world;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hello_world = findViewById(R.id.hello_world);
+        hello_world.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
